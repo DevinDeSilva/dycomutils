e889c5a6be0b2d70165da014ad1776d500d86dec
Devin De Silva <desild@rpi.edu>
2026-04-09 15:34:21 -0400

    [update] remove automation

--- dycomutils/__init__.py
+++ dycomutils/__init__.py
@@ -2,12 +2,10 @@ from . import serialization
 from . import concurrancy
 from . import grouping
 from . import config
-from . import automation
 
 __all__ = [
     'serialization',
     'concurrancy',
     'grouping',
     'config',
-    'automation',
 ]
